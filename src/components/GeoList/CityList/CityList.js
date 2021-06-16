2983f352ff1b0048f81980c8a853e2a7d2d63d49
chitra <chitraflorid@users.noreply.github.com>
2021-06-16 23:42:43 +0530

    Create CityList.js

---
+++ src/components/GeoList/CityList/CityList.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useMemo} from 'react';
+import { CityItem } from '.';
+import { Header, Search} from '../../common';
+import { GeoContext } from '..';
+
+import '../geolist.css';
+
+export const CityList = () => {
+    const type = "cities";
+    const geoContext = useContext(GeoContext);
+    const { loadCities, loading, cityList, cityFilter, setCityFilter } = geoContext;
+  
+    useEffect(() => {
+        // console.log('Initial Loading of Cities');
+        loadCities();
+    }, []);
+ 
+     return (
+        <div className="listContainer">
+            <Header type={type} />
+            <Search type={type}  handleSearch={setCityFilter} />
+        {
+            !loading ? (
+                <ul className="list">
+                    {
+                        cityList ? (
+                                cityList.map(item => {
+                                    return <CityItem 
+                                        key={item.id}
+                                        name={item.name} />
+                                })
+                            
+                        ) : (
+                            <li className="info"> No Cities!!</li>
+                        )
+                    }
+                </ul>
+            ) : (
+                <div>Loading...</div>
+            )
+        }
+    </div>
+  );
+};
