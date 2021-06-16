b1387bb1e8b89e0ff41bc93dbc8f8fcaaa814bf6
chitra <chitraflorid@users.noreply.github.com>
2021-06-16 23:41:27 +0530

    Create StateList.js

---
+++ src/components/GeoList/StateList/StateList.js
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useMemo} from 'react';
+import { StateItem } from '.';
+import { GeoContext } from '..';
+import { Header, Search} from '../../common';
+
+import '../geolist.css';
+
+export const StateList = () => {
+    const type = "states";
+    
+    const geoContext = useContext(GeoContext);
+    const {loadStates, loading, stateList, stateFilter, setStateFilter } = geoContext;
+  
+    useEffect(() => {
+         loadStates();
+    }, []);
+ 
+    return (
+        <div className="listContainer">
+            <Header type={type} />
+            <Search type={type} handleSearch={setStateFilter} />
+        {
+            !loading ? (
+                <ul className="list">
+                    {
+                       stateList ? (
+                            stateList.map(item => {
+                                return <StateItem 
+                                    key={item.id}
+                                    id={item.id}
+                                    name={item.name}
+                                    iso2={item.iso2}
+                                    cities={item.cities} />
+                                })
+                        ) : (
+                            <li className="info">No States!!</li>
+                        )
+                    }
+                </ul>
+            ) : (
+                <div>Loading...</div>
+            )
+        }
+    </div>
+  );
+};
