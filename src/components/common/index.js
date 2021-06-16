b800fa8355c3de294c892c457704f5d62928d3b8
chitra <chitraflorid@users.noreply.github.com>
2021-06-16 23:45:12 +0530

    Create index.js

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+import { Header } from './Header/Header';
+import { Search } from './Search/Search';
+import { InfoIcon, ArrowForwardIcon} from './ActionIcons';
+
+export {Header, Search, ArrowForwardIcon, InfoIcon };
