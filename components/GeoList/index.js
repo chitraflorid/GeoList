ef6ca0c25f9b0962a83b94ca624dde7a5bcef8e6
chitra <chitraflorid@users.noreply.github.com>
2021-06-16 23:29:29 +0530

    Create index.js

---
+++ components/GeoList/index.js
@@ -0,0 +1,10 @@
+import { CityList, CityItem } from './CityList';
+import { CountryList, CountryItem } from './CountryList';
+import { StateList, StateItem } from './StateList';
+
+import { GeoList } from './GeoList';
+import { GeoProvider } from './GeoProvider';
+import { GeoContext } from './context/geoContext';
+
+
+export {CityList, CityItem, CountryList, CountryItem, GeoList, GeoContext, GeoProvider, StateList, StateItem};
