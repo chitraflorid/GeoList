57255c73b383efffbcc41270f90248685e5917e1
chitra <chitraflorid@users.noreply.github.com>
2021-06-17 09:04:49 +0530

    Update StateItem.js

--- src/components/GeoList/StateList/StateItem.js
+++ src/components/GeoList/StateList/StateItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { GeoContext } from '..';
 import { InfoIcon, ArrowForwardIcon } from '../../common/ActionIcons';
 import '../listItem.css';
@@ -6,20 +6,34 @@ import '../listItem.css';
 export const StateItem = (({id, name, iso2, cities}) => {  
     const geoContext = useContext(GeoContext);
     const { loadCities, setCurrentStateId} = geoContext;
-   
-    const handleArrowClick = event => {
-        setCurrentStateId(id)
-        loadCities();
-        loadCities(cities);
+    const [active, setActive] = useState(false);
+
+    const handleStateClick = event => {       
+        if (event.currentTarget.id === id) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
+                 
+        if (event.target.closest("[name*='arrow']")) {
+            setCurrentStateId(id)
+            loadCities();
+            loadCities(cities);
+        } else {
+            console.log("Info Icon Clicked!!!!");
+        }
+        console.log(event.currentTarget.id);
+
     };
+    
   return (
-    <li className="state">
-      <div>{name}</div>
-      <div>{iso2}</div>
-      <div>
-          <InfoIcon title="State Info" />
-          <ArrowForwardIcon title="Show Cities" handleClick={handleArrowClick} />
-      </div>
+      <li id={id} className={active ? "state active": "state"}  onClick={handleStateClick}>
+          <div>{name}</div>
+          <div>{iso2}</div>
+          <div>
+              <InfoIcon title="State Info" />
+              <ArrowForwardIcon title="Show Cities" />
+          </div>
     </li>
   );
 });
