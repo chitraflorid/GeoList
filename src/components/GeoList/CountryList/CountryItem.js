3bff24ba36c28aa0098c86288a25a928e769ebe8
chitra <chitraflorid@users.noreply.github.com>
2021-06-17 09:04:17 +0530

    Update CountryItem.js

--- src/components/GeoList/CountryList/CountryItem.js
+++ src/components/GeoList/CountryList/CountryItem.js
@@ -1,4 +1,4 @@
-import React, { useContext }from 'react';
+import React, { useContext, useState }from 'react';
 import { InfoIcon, ArrowForwardIcon } from '../../common/ActionIcons';
 import { GeoContext } from '..';
 
@@ -7,23 +7,32 @@ import '../listItem.css';
 export const CountryItem = (({id, emojiU, name, iso2, states}) => {
     const geoContext = useContext(GeoContext);
     const { loadStates, loadCities, setCurrentCountryId } = geoContext;
+    const [activeId, setActive] = useState(0);
+  //  const [isActive, markActive] = useState(false);
     
     const emoji = String.fromCodePoint.apply(String, emojiU.replace(/U\+/gi, "0x").split(' '));
     
-    const handleArrowClick = event => {
-        setCurrentCountryId(id);
-        loadStates(states);
-        loadCities();
+    const handleCountryClick = event => {     
+
+            setActive(event.currentTarget.id);
+                 
+        if (event.target.closest("[name*='arrow']")) {
+            setCurrentCountryId(id);
+            loadStates(states);
+            loadCities();
+        } else {
+            console.log("Info Icon Clicked!!!!");
+        }
     };
-  
+
   return (
-      <li className="country">
+      <li id={id} className={activeId == id  ? "country active": "country"}  onClick={handleCountryClick}>
           <div>{emoji}</div>
           <div>{name}</div>
           <div>{iso2}</div>
           <div>
-              <InfoIcon name='info' title="Country Info" />
-              <ArrowForwardIcon name='arrow' title="Show States" handleClick={handleArrowClick} />
+              <InfoIcon title="Country Info" />
+              <ArrowForwardIcon title="Show States" />
           </div>
       </li>
   );
