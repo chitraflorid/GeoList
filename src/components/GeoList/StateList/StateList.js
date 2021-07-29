1353beb916df69a987cb3f9fe1c61be0b1d58883
chitra <chitraflorid@users.noreply.github.com>
2021-07-30 00:22:51 +0530

    Update StateList.js

--- src/components/GeoList/StateList/StateList.js
+++ src/components/GeoList/StateList/StateList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo} from 'react';
+import React, { useContext, useEffect } from 'react';
 import { StateItem } from '.';
 import { GeoContext } from '..';
 import { Header, Search} from '../../common';
